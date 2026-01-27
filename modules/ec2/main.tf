8b44a4f1bbc5d789f39eae16fd6ebfc2c72b8ea6
Tim Davis <tim.davis.instinct@gmail.com>
2026-01-27 12:49:22 -0600

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = var.instance_type
+  
+  monitoring = var.enable_monitoring
+  
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp3"
+  }
+  
+  tags = {
+    Name = var.instance_name
+  }
+}
